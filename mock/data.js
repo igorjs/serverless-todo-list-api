2f263e5b80be2f5469087440085e758cf7873110
Igor J. Santos <igorjosesantos@outlook.com>
2019-10-26 06:23:50 +1100

    feat(service): add mock data
    Signed-off-by: Igor J. Santos <igorjosesantos@outlook.com>

---
+++ mock/data.js
@@ -0,0 +1,26 @@
+export const mockData = [{
+  id     : '05862d73-c9f2-446e-9324-62044a49ae51',
+  created: new Date(),
+  message: 'Clean the house',
+  due    : new Date(),
+}, {
+  id     : '05862d73-c9f2-446e-9324-62044a49ae52',
+  created: new Date(),
+  message: 'Clean the house',
+  due    : new Date(),
+}, {
+  id     : '05862d73-c9f2-446e-9324-62044a49ae53',
+  created: new Date(),
+  message: 'Clean the house',
+  due    : new Date(),
+}, {
+  id     : '05862d73-c9f2-446e-9324-62044a49ae54',
+  created: new Date(),
+  message: 'Clean the house',
+  due    : new Date(),
+}, {
+  id     : '05862d73-c9f2-446e-9324-62044a49ae55',
+  created: new Date(),
+  message: 'Clean the house',
+  due    : new Date(),
+}]

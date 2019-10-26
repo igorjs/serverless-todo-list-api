5847f4e749303bc967942650c264fc359c4b6f3d
Igor J. Santos <igorjosesantos@outlook.com>
2019-10-26 11:28:00 +1100

    fix(package): update pagackes
    Signed-off-by: Igor J. Santos <igorjosesantos@outlook.com>

--- data/db.js
+++ data/db.js
@@ -1,28 +1,28 @@
 const todos = [{
-  id: '05862d73-c9f2-446e-9324-62044a49ae51',
+  id     : '05862d73-c9f2-446e-9324-62044a49ae51',
   created: new Date(),
   message: 'Clean the house 1',
-  due: new Date(),
+  due    : new Date(),
 }, {
-  id: '05862d73-c9f2-446e-9324-62044a49ae52',
+  id     : '05862d73-c9f2-446e-9324-62044a49ae52',
   created: new Date(),
   message: 'Clean the house 2',
-  due: new Date(),
+  due    : new Date(),
 }, {
-  id: '05862d73-c9f2-446e-9324-62044a49ae53',
+  id     : '05862d73-c9f2-446e-9324-62044a49ae53',
   created: new Date(),
   message: 'Clean the house 3',
-  due: new Date(),
+  due    : new Date(),
 }, {
-  id: '05862d73-c9f2-446e-9324-62044a49ae54',
+  id     : '05862d73-c9f2-446e-9324-62044a49ae54',
   created: new Date(),
   message: 'Clean the house 4',
-  due: new Date(),
+  due    : new Date(),
 }, {
-  id: '05862d73-c9f2-446e-9324-62044a49ae55',
+  id     : '05862d73-c9f2-446e-9324-62044a49ae55',
   created: new Date(),
   message: 'Clean the house 5',
-  due: new Date(),
+  due    : new Date(),
 }]
 
 export default { todos }
